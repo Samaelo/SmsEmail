d51193e925769ee4935c291d3a1b51cb8f01ca78
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-03 14:05:43 +0100

    xd

--- app/src/main/java/com/islasf/samaelmario/SmsEmail/Activity_Email.java
+++ app/src/main/java/com/islasf/samaelmario/SmsEmail/Activity_Email.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Activity_Email extends AppCompatActivity {
-
+//cagandome enla `ptuasaass
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
