457814edd0f2ddc5b9ad8f64bfe7b0f48c688ac0
Samaelo <samaelpicazo16@gmail.com>
2017-01-03 13:33:30 +0100

    He quitado el         android:fullBackupContent="true" del Manifest

---
+++ app/src/main/java/com/islasf/samaelmario/SmsEmail/Activity_Email.java
@@ -0,0 +1,13 @@
+package com.islasf.samaelmario.SmsEmail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__email);
+    }
+}
