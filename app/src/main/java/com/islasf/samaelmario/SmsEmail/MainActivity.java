b294ef96e012cfb1b8058966c511aa11090f9142
Samaelo <samaelpicazo16@gmail.com>
2017-01-03 13:06:04 +0100

    Primer commit de prueba

---
+++ app/src/main/java/com/islasf/samaelmario/SmsEmail/MainActivity.java
@@ -0,0 +1,23 @@
+package com.islasf.samaelmario.SmsEmail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
