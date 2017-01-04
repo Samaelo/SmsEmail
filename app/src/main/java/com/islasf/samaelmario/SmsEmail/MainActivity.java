1911e7c39f9c844b13bab17c941bb3aa6cb6b767
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-04 17:41:19 +0100

    Back up 04/01/2017

--- app/src/main/java/com/islasf/samaelmario/SmsEmail/MainActivity.java
+++ app/src/main/java/com/islasf/samaelmario/SmsEmail/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        EnvioMensajes envio = new EnvioMensajes(this);
+        envio.verificar_permisos(this);
+    }
+    public void onSMS(View v){
+        EnvioMensajes envio = new EnvioMensajes(this);
+        envio.enviar_SMS("Enviado desde Android StudioeeEnviado\"Enviado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee d\"Envi\"Enviado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","");
+    }
+    public void onMAIL(View v){
+        EnvioMensajes envio = new EnvioMensajes(this);
+        envio.enviar_email("desde android studio ermanox","");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
