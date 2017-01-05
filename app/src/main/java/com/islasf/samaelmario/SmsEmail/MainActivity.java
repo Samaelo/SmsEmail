fc52ccf03f46085bcebd72ea322ba71b76ed4051
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-05 12:52:00 +0100

    Modificando.

--- app/src/main/java/com/islasf/samaelmario/SmsEmail/MainActivity.java
+++ app/src/main/java/com/islasf/samaelmario/SmsEmail/MainActivity.java
@@ -1,10 +1,13 @@
 package com.islasf.samaelmario.SmsEmail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+
+import com.islasf.samaelmario.tuputamadre.ActivitySMS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,18 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        startActivity(new Intent(this, ActivitySMS.class));
+    }
 
-        EnvioMensajes envio = new EnvioMensajes(this);
-        envio.verificar_permisos(this);
-    }
-    public void onSMS(View v){
-        EnvioMensajes envio = new EnvioMensajes(this);
-        envio.enviar_SMS("Enviado desde Android StudioeeEnviado\"Enviado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee d\"Envi\"Enviado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeado desde Android StudioeeEnviado desde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesde Android Studioeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","");
-    }
-    public void onMAIL(View v){
-        EnvioMensajes envio = new EnvioMensajes(this);
-        envio.enviar_email("desde android studio ermanox","");
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
