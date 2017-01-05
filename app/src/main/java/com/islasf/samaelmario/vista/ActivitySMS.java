2eaccfaae6c6ee8a7166524f1c5aee2cc2a7481e
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-05 15:54:17 +0100

    Modificando interfaz.2

--- app/src/main/java/com/islasf/samaelmario/vista/ActivitySMS.java
+++ app/src/main/java/com/islasf/samaelmario/vista/ActivitySMS.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class ActivitySMS extends AppCompatActivity {
@@ -13,17 +15,30 @@ public class ActivitySMS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+
+        //Cargamos el actionBar
+
+
+    }
+    private void cargar_actionBar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onEnviar(View v) {
+        Snackbar.make(v, "Aquí va el mensaje de enviado o no.", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+    public void onCancelar(View v) {
+        Snackbar.make(v, "Aquí va el mensaje de \"Mensaje cancelado\".", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 }
--- app/src/main/java/com/islasf/samaelmario/vista/MainActivity.java
+++ app/src/main/java/com/islasf/samaelmario/vista/MainActivity.java
@@ -1,10 +1,14 @@
 package com.islasf.samaelmario.vista;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 /*
@@ -16,12 +20,21 @@ TODO: Puesto que hay que poner preferencias, vamos a poner que una de ellas sea 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //   Personalización del ActionBar   //
+        cargar_actionBar();
+
+        startActivity(new Intent(this, ActivitySMS.class));
+
+
+    }
+
+    private void cargar_actionBar(){
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-        startActivity(new Intent(this, ActivitySMS.class));
+        getSupportActionBar().setTitle("");
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
