e0aa6cfb8dd8f367d8d72e761a49056040d3788c
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-06 12:33:56 +0100

    Modificando interfaz.4

--- app/src/main/java/com/islasf/samaelmario/vista/ListaContactosActivity.java
+++ app/src/main/java/com/islasf/samaelmario/vista/ListaContactosActivity.java
@@ -18,7 +18,7 @@ public class ListaContactosActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAgregarContacto);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
