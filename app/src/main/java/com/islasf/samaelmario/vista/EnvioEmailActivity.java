f34050e4daf518bf2628f9e2b510db0cbab5720f
geraldoriviarino <mariogarciarb@gmail.com>
2017-01-30 14:19:56 +0100

    Envío  y listado de mail y SMS, preferencias, perfil terminados.

--- app/src/main/java/com/islasf/samaelmario/vista/EnvioEmailActivity.java
+++ app/src/main/java/com/islasf/samaelmario/vista/EnvioEmailActivity.java
@@ -58,7 +58,7 @@ public class EnvioEmailActivity extends AppCompatActivity implements Funcionalid
 
     }
 
-      //////////////////////////////////
+      ///////////////////////////////////
      //      Carga de elementos      //
     //////////////////////////////////
 
